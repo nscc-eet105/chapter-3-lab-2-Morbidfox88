98eab007d9c559a78c3e12467e30a40bc2d5eddd
Chad Collard <chadcollard1567@gmail.com>
2025-06-24 15:22:54 -0400

    Chapter 3 lab 2 done

--- chapter 3 lab 2.py
+++ chapter 3 lab 2.py
@@ -2,26 +2,22 @@
 #Chapter 3 Lab 2
 #6/22/25
 
-print ("                       Jane's Stuff Store")
+print ("\t\t\t\tJane's Stuff Store")
 
 
 items = int(input ("please enter which items you would like to purchase (1-3): "))
 
 
-if items == 1:
-    print("Item 1 = 1.50")
-elif items == 2:
-    print("Item 2 = 3.45")
-elif items == 3:
-    print("Item 3 = 2.00")
-else:
-    print("please make another selection")
-
 total = 0
-for items in range(1, items):
-    print(items)
-    total += items
+for i in range(items):
+    print(f"enter the price of item {i+1}:",end=" ")
+    price = float(input())
+    total += price
+average=total/items    
 
-print(f"The total is {total}.")
+
+print(f"The total cost of your items is {total}")
+print(f"The average cost of each item is {average}")
+
     
     
